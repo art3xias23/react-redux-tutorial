0356d2444852533ed48a4e438aafa94fcc1c4e24
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-04 22:33:45 +0000

    Added Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,20 @@
-import {legacy_createStore as createStore} from 'redux';
+import {configureStore, createSlice} from '@reduxjs/toolkit';
 
-const reducerFn = (state = {counter: 0}, action) =>{
-    // Needs to be a synchronous function
-    // We should not mutate the original state
-
-    if(action.type == "INC"){
-        return {counter: state.counter+1}
+const counterSlice = createSlice({
+    name:"counter",
+    initialState:{counter:0},
+    reducers:{
+        increment(state,action){
+            state.counter = state.counter +1
+        },
+        decrement(state,action){
+            state.counter = state.counter -1
+        },
+        addBy(state,action){
+            state.counter = state.counter + action.payload
+        }
     }
-    else if(action.type =="DEC"){
-        return {counter: state.counter - 1}
-    }
-    else if(action.type =="ADD")
-    {
-        return {counter : state.counter + action.payload}
-    }
-
-    return state;
-}
-const store = createStore(reducerFn);
-
+})
+export const actions = counterSlice.actions;
+const store = configureStore({reducer: counterSlice.reducer});
 export default store;
