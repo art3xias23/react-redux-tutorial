d5efb79f4e64a3b9e88b6c7557bab631c448a8bd
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-04 19:29:17 +0000

    Created basic redux data store

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import {legacy_createStore as createStore} from 'redux';
+
+const reducerFn = (state = {counter: 10}, action) =>{
+    return state;
+}
+const store = createStore(reducerFn);
+
+export default store;
