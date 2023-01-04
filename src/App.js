54114cd63fad49f02b5a6e494b2d419a3fef7097
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2023-01-04 19:38:07 +0000

    Added increment dispatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const increment = () => {
 
+  const counter = useSelector((state) => state.counter);
+const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({type:'INC'});
   }
   
   const decrement = () => {
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
 import {legacy_createStore as createStore} from 'redux';
 
-const reducerFn = (state = {counter: 10}, action) =>{
+const reducerFn = (state = {counter: 0}, action) =>{
+    // Needs to be a synchronous function
+    // We should not mutate the original state
+
+    if(action.type == "INC"){
+        return {counter: state.counter+1}
+    }
+
     return state;
 }
 const store = createStore(reducerFn);
